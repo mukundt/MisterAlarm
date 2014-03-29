0f9cedb7b29aec181dca472d050d6be72bd0c8b1
tiansjin <tiansjin@gmail.com>
2014-03-29 00:17:51 -0400

    sensor

--- Arduino/Alarm_Main/Alarm_Main.ino
+++ Arduino/Alarm_Main/Alarm_Main.ino
@@ -1,11 +1,16 @@
+#define PIN_SENSOR 7
+
+#include <SPI.h>
 #include <boards.h>
 #include <ble_shield.h>
 #include <services.h>
 
-const int pingPin = 7;
 boolean check = false;
+boolean prevcheck = false;
+int dur = 0;
 
 void setup() {
+  //ble_begin();
   Serial.begin(9600);
 }
 
@@ -13,26 +18,29 @@ void loop()
 {
   long duration, cm;
   
-  pinMode(pingPin, OUTPUT);
-  digitalWrite(pingPin, LOW);
+  pinMode(PIN_SENSOR, OUTPUT);
+  digitalWrite(PIN_SENSOR, LOW);
   delayMicroseconds(2);
-  digitalWrite(pingPin, HIGH);
+  digitalWrite(PIN_SENSOR, HIGH);
   delayMicroseconds(5);
-  digitalWrite(pingPin, LOW);
+  digitalWrite(PIN_SENSOR, LOW);
   
-  pinMode(pingPin, INPUT);
-  duration = pulseIn(pingPin, HIGH);
+  pinMode(PIN_SENSOR, INPUT);
+  duration = pulseIn(PIN_SENSOR, HIGH);
   
   cm = duration / 29 /2;
-  
-  delay(150);
+  prevcheck = check;
+  delay(50);
   if (cm < 40){
     check = true;
+    dur++;
   }
   else {
     check = false;
+    dur = 0;
   }
-  if (check){
+  if (check && !prevcheck){
+    //ble_write('c');
     Serial.println("HI MUKUND");
   }
 
