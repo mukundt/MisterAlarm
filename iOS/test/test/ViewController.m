779ae542161c5faa1ffde2e61b3860a67e2de3ef
Ashley Lai <ahlai@cmu.edu>
2014-03-29 17:45:32 -0400

    working on calendar

--- iOS/test/test/ViewController.m
+++ iOS/test/test/ViewController.m
@@ -14,7 +14,6 @@
 #import "AVFoundation/AVAudioPlayer.h"
 
 
-
 @interface ViewController ()
 
 @end
@@ -34,12 +33,12 @@ GTMOAuth2Authentication *auth;
 
 
 - (NSString*) getCalendarEvents
-{
+{ // Returns json-formatted string of calendar events
     
     NSString *urlStr = @"https://www.googleapis.com/calendar/v3/calendars/countableirrationals%40gmail.com/events?";
     NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:urlStr]];
     [request setHTTPMethod:@"GET"];
-    
+    __block NSMutableString *outp;
     [auth authorizeRequest:request
               completionHandler:^(NSError *error) {
                   NSString *output = nil;
@@ -47,7 +46,6 @@ GTMOAuth2Authentication *auth;
                       output = [error description];
                   } else {
                       // Synchronous fetches like this are a really bad idea in Cocoa applications
-                      //
                       // For a very easy async alternative, we could use GTMHTTPFetcher
                       NSURLResponse *response = nil;
                       NSData *data = [NSURLConnection sendSynchronousRequest:request
@@ -57,26 +55,66 @@ GTMOAuth2Authentication *auth;
                           // API fetch succeeded
                           output = [[NSString alloc] initWithData:data
                                                           encoding:NSUTF8StringEncoding] ;
-                          
                           NSLog(@"output:%@",output);
-                          
+                          outp = output;
                           
                       } else {
                           // fetch failed
                           output = [error description];
                       }
                   }
-                  
               }];
-    
-    NSString *responseString = [[NSString alloc] initWithData:responseData encoding:NSUTF8StringEncoding];
-    //data =nil;
-    
-    NSArray *jsonArray = [responseString JSONValue];
-    
-    return output;
-
+    return outp;
 }
+
+- (NSData*) stringToData:(NSString*) calendar_info
+{   // Converts json string format to data
+    NSData* data = [calendar_info dataUsingEncoding:NSUTF8StringEncoding];
+    return data;
+}
+
+- (NSDictionary*) dataToDictionary:(NSData*)calendar_info
+{
+    // Create NSDictionary from the JSON data
+    NSDictionary *jsonDictionary = [NSJSONSerialization JSONObjectWithData:calendar_info options:0 error:nil];
+    return jsonDictionary;
+}
+
+- (NSMutableArray*) getSummary:(NSDictionary*)calendar_info
+{
+    // Returns array of events on calendar
+    NSMutableArray* full_summary = [[NSMutableArray alloc] init];
+    // Create array of dictionaries with key "items"
+    NSArray* all_events = [calendar_info objectForKey:@"items"];
+    // Create array of strings with key "summary";
+    for (NSDictionary *event in all_events)
+    {
+        NSString *summary = [event objectForKey:@"summary"];
+        [full_summary addObject: summary];
+        NSLog(@"Please work!");
+    }
+    return full_summary; // Returns summary of strings
+}
+
+/*- (NSMutableArray*) jSon_to_eventArray:(NSDictionary *)calendar_info
+{
+    //create an empty string array
+    NSMutableArray *item_array= [NSMutableArray array];
+    NSMutableArray *event_array= [NSMutableArray array];
+    //loop through events in JSON
+    NSLog(@"is this getting here");
+    for (NSDictionary* event in calendar_info){
+        NSArray *item= [event objectForKey:@"items"];
+        [item_array addObject:item];
+        NSLog(@"Everything is error!");
+        NSLog(item);
+    }
+    NSLog(@"how about here?");
+    //for each event, add the string under even summary to the array
+    //return the array
+    return (event_array);
+    
+} */
 
 - (void) setup
 {
@@ -110,26 +148,7 @@ GTMOAuth2Authentication *auth;
 
         
     }
-    [self jSon_to_eventArray:[self getCalendarEvents]];
-}
-
-
-
-- (NSMutableArray*)jSon_to_eventArray:(NSString *)calendar_info
-{
-    //create an empty string array
-    NSMutableArray *item_array= [NSMutableArray array];
-    NSMutableArray *event_array= [NSMutableArray array];
-    //loop through events in JSON
-    for (NSDictionary* event in calendar_info){
-        NSString *item= @"items";
-        [item_array addObject:item];
-        NSLog(item);
-    }
-    //for each event, add the string under even summary to the array
-    //return the array
-    return (event_array);
-    
+    [self getSummary:[self dataToDictionary:[self stringToData:[self getCalendarEvents]]]];
 }
 
 AVAudioPlayer *audioPlayer;
